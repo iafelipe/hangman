b629333f9ae6f921e68499a2baa0c816e3086546
Igor Felipe <47894612+iafelipe@users.noreply.github.com>
2024-01-29 18:51:21 -0300

    Refactor hangman game to use a separate words module

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -7,6 +7,7 @@ from rich.console import Console
 from rich.prompt import Confirm, Prompt
 
 from .ui import display
+from .words import words
 
 ALPHABET = [letter for letter in string.ascii_uppercase]
 
@@ -28,70 +29,63 @@ def main() -> None:
     try:
         console = Console()
 
-        with open("words.txt", "r", encoding="utf-8") as f:
-            possible_words = f.read().splitlines()
+        while True:
+            word = random.choice(words).upper()
+            guess = ["__"] * len(word)
+            wrong_letters = []
+            rich_letters = {c: c for c in ALPHABET}
+
+            count_wrong = 0
+            count_right = 0
+            valid_char = True
+            repeated_letter = False
 
             while True:
-                word = random.choice(possible_words).upper()
-                guess = ["__"] * len(word)
-                wrong_letters = []
-                rich_letters = {c: c for c in ALPHABET}
+                clear_screen()
+                display(console, count_wrong, guess, rich_letters)
 
-                count_wrong = 0
-                count_right = 0
-                valid_char = True
-                repeated_letter = False
-
-                while True:
-                    clear_screen()
-                    display(console, count_wrong, guess, rich_letters)
-
-                    if count_wrong == 6:
-                        console.print("[red]You lost! :cry::2nd_place_medal:")
-                        break
-
-                    if count_right == len(word):
-                        console.print("[green]You win! :smiley::1st_place_medal:")
-                        break
-
-                    if not valid_char:
-                        console.print("[prompt.invalid]Please enter a valid letter!")
-
-                    if repeated_letter:
-                        console.print(
-                            "[prompt.invalid]You've already entered this letter!"
-                        )
-
-                    char = Prompt.ask("Insert a letter [cyan](-q to quit)").upper()
-
-                    if char == "-Q":
-                        break
-
-                    valid_char = char in ALPHABET
-
-                    if valid_char:
-                        if char in guess or char in wrong_letters:
-                            repeated_letter = True
-
-                        else:
-                            if char in word:
-                                for i in search_char(char, word):
-                                    count_right += 1
-                                    guess[i] = char
-                                    rich_letters[
-                                        char
-                                    ] = f"[bold green]{char}[/bold green]"
-
-                            else:
-                                count_wrong += 1
-                                wrong_letters.append(char)
-                                rich_letters[char] = f"[bold red]{char}[/bold red]"
-
-                restart = Confirm.ask("Wanna play again?", default=True)
-                if not restart:
+                if count_wrong == 6:
+                    console.print("[red]You lost! :cry::2nd_place_medal:")
                     break
 
-                clear_screen()
+                if count_right == len(word):
+                    console.print("[green]You win! :smiley::1st_place_medal:")
+                    break
+
+                if not valid_char:
+                    console.print("[prompt.invalid]Please enter a valid letter!")
+
+                if repeated_letter:
+                    console.print("[prompt.invalid]You've already entered this letter!")
+
+                char = Prompt.ask("Insert a letter [cyan](-q to quit)").upper()
+
+                if char == "-Q":
+                    break
+
+                valid_char = char in ALPHABET
+
+                if valid_char:
+                    if char in guess or char in wrong_letters:
+                        repeated_letter = True
+
+                    else:
+                        if char in word:
+                            for i in search_char(char, word):
+                                count_right += 1
+                                guess[i] = char
+                                rich_letters[char] = f"[bold green]{char}[/bold green]"
+
+                        else:
+                            count_wrong += 1
+                            wrong_letters.append(char)
+                            rich_letters[char] = f"[bold red]{char}[/bold red]"
+
+            restart = Confirm.ask("Wanna play again?", default=True)
+            if not restart:
+                break
+
+            clear_screen()
 
     except KeyboardInterrupt:
         console.print(" [yellow]See you later! :wave::smiley:")
